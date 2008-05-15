acee378c9ef246ae3a2622835a8b7903ed70e67f
François Beausoleil <francois@teksol.info>
2008-05-15 16:28:42 -0400

    Untested (but should be working, yeah right) #add_project, #add_token, #remove_token in GlobalConfig.

--- lib/fogbugz_offline/global_config.rb
+++ lib/fogbugz_offline/global_config.rb
@@ -14,6 +14,26 @@ module FogbugzOffline
       end
     end
 
+    def add_project(project_url)
+      projects[project_url] = Array.new
+    end
+
+    def add_token(project_url, token)
+      project(project_url) << token
+    end
+
+    def remove_token(project_url, token)
+      project(project_url).delete(token)
+    end
+
+    def projects
+      @config["projects"] ||= Array.new
+    end
+
+    def project(url)
+      projects[project_url] ||= []
+    end
+
     def write
       @path.dirname.mkdir unless @path.dirname.directory?
       File.open(@path, "wb") do |io|
